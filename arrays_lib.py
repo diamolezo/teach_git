7092641322cf51d1dbcef456c02e27ec7bf43ea8
diamolezo <diamolezo@gmail.com>
2025-05-24 21:32:09 +0300

    FEAT(arrays_lib): Добавлены функции filter_greater и filter_less

--- arrays_lib.py
+++ arrays_lib.py
@@ -1,1 +1,10 @@
-git
+def filter_greater(arr, value):
+    ''' возвращает массив из элементов `arr`,
+    которые больше `value`.'''
+    pass
+
+
+def filter_less(arr, value):
+    ''' возвращает массив из элементов
+     `arr`, которые меньше `value`. '''
+    pass

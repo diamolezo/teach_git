b3b74cfe39670b6287e0e464b6711fdd293a5d88
Yuriy Terlyanskiy <boxtersh@yandex.ru>
2025-05-25 20:12:51 +0300

    feat (function) filter_not_equal

--- arrays_lib.py
+++ arrays_lib.py
@@ -29,3 +29,19 @@ def filter_less_11111111(arr: list, value:int) -> list:
             array_elem_smal_value.append(arr[i])
 
     return array_elem_smal_value
+
+
+def filter_not_equal(arr: list, value:int) -> list:
+    """
+
+    :param arr: list[]
+    :param value: int
+    :return: list[] array elements < value
+    """
+    array_elem_not_equal_value = []
+
+    for i in range(len(arr)):
+        if arr[i] != value:
+            array_elem_not_equal_value.append(arr[i])
+
+    return array_elem_not_equal_value
